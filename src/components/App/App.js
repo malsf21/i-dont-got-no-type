cbb62ef14a0d730017ca01c77389ae89be8f2dc3
Matthew Wang <matt@matthewwang.me>
2019-06-16 17:54:52 -0700

    adds case insensitive option

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       correct: 0,
       incorrect: 0,
       size: 100,
+      lowercaseOnly: false,
       wrongAllowed: true,
       words: []
     }
@@ -27,7 +28,39 @@ class App extends Component {
       )
     }
   }
+  generateNewSong = () => {
+    console.log(this.state)
+    let newCurrentSong = data[Math.floor(Math.random() * data.length)];
+    let tempWords = newCurrentSong.lyrics.split(" ");
+    let startIndex = Math.floor(Math.random() * (tempWords.length-100));
+    let words = []
+    for (let i = startIndex; i < startIndex + this.state.size; i++){
+      let word = tempWords[i];
+      if (this.state.lowercaseOnly){
+        word = word.toLowerCase();
+      }
+      words.push(
+        {
+          "word": word,
+          "correct": false
+        }
+      )
+    }
+    this.setState({
+      currentSong: newCurrentSong,
+      words: words,
+      currentWord: 0,
+      correct: 0,
+      incorrect: 0,
+    })
+  }
+  changeLowercaseOnly = () => {
+    this.setState({lowercaseOnly: !this.state.lowercaseOnly}, () => this.generateNewSong()); // required bc. state change is async
+  }
   checkWord = guess => {
+    if (this.state.lowercaseOnly){
+      guess = guess.toLowerCase();
+    }
     if (this.state.correct + this.state.incorrect < this.state.size){
       let correctBool = true;
       if (guess !== this.state.words[this.state.currentWord].word){
@@ -51,6 +84,16 @@ class App extends Component {
         <div className="app-container">
           <h1 className="text-tight">i don't got no type</h1>
           <h4 className="text-tight">a typing game focused on rap lyrics</h4>
+          <hr className="divider" />
+          <div>
+            <span className="input-group">
+              <input
+                type="checkbox"
+                onChange={this.changeLowercaseOnly}
+              />
+              case insensitive
+            </span>
+          </div>
           <div className="row">
             <div className="col text-left">
               {this.state.currentSong.title} - {this.state.currentSong.artist}
