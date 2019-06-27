5ce749cb06a4b6ea97e6a03544068a10ede31173
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-27 04:29:32 +0000

    Deploy malsf21/i-dont-got-no-type to github.com/malsf21/i-dont-got-no-type.git:gh-pages

---
+++ precache-manifest.3099e90a2ddf6b4c5a18a078a0381d5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f94a4eabb65771bdbd343527ca18dc7",
+    "url": "/i-dont-got-no-type/index.html"
+  },
+  {
+    "revision": "a0968eba4c1173cb7d28",
+    "url": "/i-dont-got-no-type/static/css/main.1960d005.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/i-dont-got-no-type/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "a0968eba4c1173cb7d28",
+    "url": "/i-dont-got-no-type/static/js/main.b5c9ec54.chunk.js"
+  },
+  {
+    "revision": "33a924af1905bdb04060",
+    "url": "/i-dont-got-no-type/static/js/runtime~main.6b1f0885.js"
+  }
+]);
